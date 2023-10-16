97f28623b6b385f39d101a0cf1713c2809c1f1e9
Greg <grzegorz.lapinski@live.com>
2023-10-16 10:38:44 -0700

    update

--- .github/workflows/config_registry.jl
+++ .github/workflows/config_registry.jl
@@ -1,4 +1,4 @@
 using Pkg;
-Pkg.add(url="https://github.com/grlap/OpenSSL.jl")
-Pkg.add(url="https://github.com/grlap/Nghttp2.jl")
+Pkg.develop(url="https://github.com/grlap/OpenSSL.jl");
+Pkg.develop(url="https://github.com/grlap/Nghttp2.jl");
 exit()
